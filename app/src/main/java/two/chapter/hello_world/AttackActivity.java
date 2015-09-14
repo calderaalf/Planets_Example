465590de105f0e496da3997b7195eec2a7fc88a9
calderaalf <alfredcaldera@hotmail.com>
2015-09-13 21:38:22 -0430

    Finalizado capitulo 15 del libro

--- app/src/main/java/two/chapter/hello_world/AttackActivity.java
+++ app/src/main/java/two/chapter/hello_world/AttackActivity.java
@@ -1,9 +1,13 @@
 package two.chapter.hello_world;
 
+import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.SparseIntArray;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,18 +18,27 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 
 public class AttackActivity extends ActionBarActivity {
-    private MediaPlayer bombPlayer = null;
-    private MediaPlayer transportPlayer = null;
-    private MediaPlayer virusPlayer = null;
-    private MediaPlayer laserPlayer = null;
+    private SoundPool soundPoolFx;
+    private SparseIntArray soundPoolMap;
+    int sample1 = 1;
+    int sample2 = 2;
+    int sample3 = 3;
+    int sample4 = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attack);
-        setAudioPlayers();
+        soundPoolFx = new SoundPool(4, AudioManager.STREAM_MUSIC,100);
+        soundPoolMap = new SparseIntArray(4);
+        soundPoolMap.append(sample1, soundPoolFx.load(this, R.raw.blast, 1));
+        soundPoolMap.append(sample2, soundPoolFx.load(this, R.raw.transport, 1));
+        soundPoolMap.append(sample3, soundPoolFx.load(this, R.raw.virus, 1));
+        soundPoolMap.append(sample4, soundPoolFx.load(this, R.raw.laser, 1));
 
         ImageButton bombButton = (ImageButton)findViewById(R.id.bombButton);
         Animation rotateBomb  = AnimationUtils.loadAnimation(this,R.anim.anim_rot_bomb);
@@ -34,7 +47,6 @@ public class AttackActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(AttackActivity.this, "Bombs Away!!", Toast.LENGTH_SHORT).show();
-                bombPlayer.start();
             }
         });
 
@@ -48,7 +60,7 @@ public class AttackActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(AttackActivity.this,"Troops Sent!",Toast.LENGTH_SHORT).show();
-                transportPlayer.start();
+                playSample(sample2,1.0f);
             }
         });
 
@@ -60,7 +72,8 @@ public class AttackActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(AttackActivity.this,"Virus Spread!",Toast.LENGTH_SHORT).show();
-                virusPlayer.start();
+                playSample(sample3, 1.0f);
+
             }
         });
 
@@ -71,7 +84,8 @@ public class AttackActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(AttackActivity.this,"Laser Fired!",Toast.LENGTH_SHORT).show();
-                laserPlayer.start();
+                playSample(sample4,1.0f);
+
             }
         });
 
@@ -114,12 +128,14 @@ public class AttackActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+        public void playSample(int sample, float pitchShift){
+            AudioManager manageAudio = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+            float curSampleVolume =  manageAudio.getStreamVolume(AudioManager.STREAM_MUSIC);
+            float maxSampleVolume = manageAudio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            float setSampleVolume = curSampleVolume / maxSampleVolume;
+            soundPoolFx.play(soundPoolMap.get(sample), setSampleVolume, setSampleVolume,0,0,pitchShift);
 
-    private void setAudioPlayers(){
-        bombPlayer = MediaPlayer.create(getApplicationContext(),R.raw.blast);
-        transportPlayer = MediaPlayer.create(getApplicationContext(),R.raw.transport);
-        virusPlayer = MediaPlayer.create(getApplicationContext(),R.raw.virus);
-        laserPlayer = MediaPlayer.create(getApplicationContext(),R.raw.laser);
+        }
 
-    }
+
 }
